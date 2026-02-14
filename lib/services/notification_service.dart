b6c33e0649eba9cfe16a2d32641461f962b388bb
Caner Erdem <mcanererdem@gmail.com>
2026-02-14 21:24:52 +0300

    fix: Update Android SDK to 35 and remove deprecated notification parameter

--- lib/services/notification_service.dart
+++ lib/services/notification_service.dart
@@ -60,8 +60,6 @@ class NotificationService {
           ),
         ),
         androidScheduleMode: AndroidScheduleMode.exactAllowWhileIdle,
-        uiLocalNotificationDateInterpretation:
-            UILocalNotificationDateInterpretation.absoluteTime,
         matchDateTimeComponents: DateTimeComponents.time,
       );
       
